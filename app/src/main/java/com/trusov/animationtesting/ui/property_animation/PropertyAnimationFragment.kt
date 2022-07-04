6d321285d309ea21938c8271afd0f24bebb47faf
Glebasta333 <gleb.trusow@yandex.ru>
2022-07-04 11:03:30 +0300

    add rotate animation to textView

--- app/src/main/java/com/trusov/animationtesting/ui/property_animation/PropertyAnimationFragment.kt
+++ app/src/main/java/com/trusov/animationtesting/ui/property_animation/PropertyAnimationFragment.kt
@@ -1,16 +1,15 @@
 package com.trusov.animationtesting.ui.property_animation
 
+import android.animation.Animator
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.animation.Animation
-import android.view.animation.DecelerateInterpolator
-import android.view.animation.LinearInterpolator
-import android.view.animation.RotateAnimation
+import android.view.animation.*
 import android.widget.TextView
+import androidx.core.view.ViewCompat.animate
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.trusov.animationtesting.databinding.FragmentPropertyAnimationBinding
@@ -41,15 +40,13 @@ class PropertyAnimationFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val imageView = binding.ivImage
-
-        val animation = RotateAnimation(0F, 350F, 15F, 15F).apply {
-            interpolator = LinearInterpolator()
-            repeatCount = Animation.INFINITE
-            duration = 700L
+        binding.textHome.setOnClickListener {
+            it.animate().apply {
+                rotation(360F)
+                duration = 1000L
+                interpolator = AccelerateDecelerateInterpolator()
+            }.start()
         }
-
-        imageView.startAnimation(animation)
     }
 
     override fun onDestroyView() {
